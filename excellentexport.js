cd40c5b525dd53b262fb09c474220373fc151c29
jmaister <jordimaister@gmail.com>
2013-11-26 01:52:29 +0100

    Make excel work with links.

--- excellentexport.js
+++ excellentexport.js
@@ -6,7 +6,6 @@
  * http://jsfiddle.net/insin/cmewv/
  *  
  */
-
 window.ExcellentExport = (function() {
     var uri = 'data:application/vnd.ms-excel;base64,';
     var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>';
@@ -19,29 +18,19 @@ window.ExcellentExport = (function() {
         });
     };
 
-    var linkTemplate = '<a download="{filename}" href="data:application/csv;base64,{data}">{text}</a>';
-    var csvData = "col1,col2,col3\n100,200,300\n123,456,789";
-
     var ee = {
-        excel: function(table, name) {
+        excel: function(anchor, table, name) {
             if (!table.nodeType) {
                 table = document.getElementById(table);
             }
             var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML};
-            console.log(uri + base64(format(template, ctx)));
-            window.location.href = uri + base64(format(template, ctx));
-        },
-        csv: function(table) {
-            link = 'data:application/csv;charset=UTF-8,' + encodeURIComponent(csvData);
-            console.log(link);
-            window.location.href = link;
-        },
-        csv64: function(table) {
-            link = 'data:application/csv;base64,' + base64(csvData);
-            console.log(link);
-            window.location.href = link;
-        } 
-   };
+            var hrefvalue = uri + base64(format(template, ctx));
+            console.log(hrefvalue);
+            anchor.href = hrefvalue;
+            // Return true to allow the link to work
+            return true;
+        }
+    };
 
     return ee;
 }());
