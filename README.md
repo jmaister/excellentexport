fe140b58252d53637e46d9c52e9984ceb7d39414
jmaister <jordimaister@gmail.com>
2013-11-27 21:00:00 +0100

    Add CSV data format.

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 ExcellentExport.js
 ==================
 
-Javascript export to Excel
+Javascript export to Excel or CSV
 
-A quick Javascript librery to create export to Excel from HTML tables automatically in the browser. No server required.
+A quick Javascript librery to create export to Excel/CSV from HTML tables automatically in the browser. No server required.
 
 Check my blog page for testing:
 [Javascript export to Excel](http://jordiburgos.com/post/2013/javascript-export-to-excel.html)
+
+Usage
+=====
+
+    <table id="datatable">
+        <tr>
+            <td>100</td> <td>200</td> <td>300</td>
+        </tr>
+        <tr>
+            <td>400</td> <td>500</td> <td>600</td>
+        </tr>
+    </table>
+
+    <a download="somedata.xls" href="#" onclick="return ExcellentExport.excel(this, 'datatable', 'Sheet Name Here');">Export to Excel</a>
+    <a download="somedata.csv" href="#" onclick="return ExcellentExport.csv(this, 'datatable');">Export to CSV</a>
+
+Revision history:
+
+1.0 Added Excel data export
+1.1 Added CSV data export
--- excellentexport.js
+++ excellentexport.js
@@ -1,5 +1,8 @@
 /*
- * ExcellentExport
+ * ExcellentExport.
+ * A client side Javascript export to Excel.
+ * 
+ * @autor: Jordi Burgos (jordiburgos@gmail.com)
  * 
  * Based on:
  * https://gist.github.com/insin/1031969
@@ -7,8 +10,9 @@
  *  
  */
 window.ExcellentExport = (function() {
-    var uri = 'data:application/vnd.ms-excel;base64,';
-    var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>';
+    var version = "1.1";
+    var uri = { excel: 'data:application/vnd.ms-excel;base64,', csv: 'data:application/csv;base64,' };
+    var template = { excel: '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>' };
     var base64 = function(s) {
         return window.btoa(unescape(encodeURIComponent(s)));
     };
@@ -18,16 +22,31 @@ window.ExcellentExport = (function() {
         });
     };
 
+    var get = function(element) {
+        if (!element.nodeType) {
+            return document.getElementById(element);
+        }
+        return element;
+    };
+
+    var tableToCSV = function(table) {
+        return "COL1,COL2,COL3\n100,200,300\n400,500,600";
+    };
+
     var ee = {
         excel: function(anchor, table, name) {
-            if (!table.nodeType) {
-                table = document.getElementById(table);
-            }
+            table = get(table);
             var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML};
-            var hrefvalue = uri + base64(format(template, ctx));
+            var hrefvalue = uri.excel + base64(format(template.excel, ctx));
             anchor.href = hrefvalue;
             // Return true to allow the link to work
             return true;
+        },
+        csv : function(anchor, table) {
+            table = get(table);
+            var csvData = tableToCSV(table);
+            var hrefvalue = uri.csv + base64(csvData);
+            anchor.href = hrefvalue;
         }
     };
 
