4c2f07da1564e8bec981702aa8e242b46d7a6eca
jmaister <jordimaister@gmail.com>
2014-02-18 23:28:10 +0100

    Update Base64 encode/decode.

--- excellentexport.js
+++ excellentexport.js
@@ -11,95 +11,82 @@
  * CSV: http://en.wikipedia.org/wiki/Comma-separated_values
  */
 
-
 /*
- * Base64 encoder from: http://ntt.cc/2008/01/19/base64-encoder-decoder-with-javascript.html 
+ * Base64 encoder/decoder from: http://jsperf.com/base64-optimized
  */
- 
-var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
-function encode64(input) {
-    input = escape(input);
-    var output = "";
-    var chr1, chr2, chr3 = "";
-    var enc1, enc2, enc3, enc4 = "";
-    var i = 0;
 
-    do {
-        chr1 = input.charCodeAt(i++);
-        chr2 = input.charCodeAt(i++);
-        chr3 = input.charCodeAt(i++);
+var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+var fromCharCode = String.fromCharCode;
+var INVALID_CHARACTER_ERR = ( function() {
+        // fabricate a suitable error object
+        try {
+            document.createElement('$');
+        } catch (error) {
+            return error;
+        }
+    }());
 
-        enc1 = chr1 >> 2;
-        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-        enc4 = chr3 & 63;
+// encoder
+window.btoa || (window.btoa = function(string) {
+    var a, b, b1, b2, b3, b4, c, i = 0, len = string.length, max = Math.max, result = '';
 
-        if (isNaN(chr2)) {
-            enc3 = enc4 = 64;
-        } else if (isNaN(chr3)) {
-            enc4 = 64;
+    while (i < len) {
+        a = string.charCodeAt(i++) || 0;
+        b = string.charCodeAt(i++) || 0;
+        c = string.charCodeAt(i++) || 0;
+
+        if (max(a, b, c) > 0xFF) {
+            throw INVALID_CHARACTER_ERR;
         }
 
-        output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4);
-        chr1 = chr2 = chr3 = "";
-        enc1 = enc2 = enc3 = enc4 = "";
-    } while (i < input.length);
+        b1 = (a >> 2) & 0x3F;
+        b2 = ((a & 0x3) << 4) | ((b >> 4) & 0xF);
+        b3 = ((b & 0xF) << 2) | ((c >> 6) & 0x3);
+        b4 = c & 0x3F;
 
-    return output;
-}
-
-function decode64(input) {
-    var output = "";
-    var chr1, chr2, chr3 = "";
-    var enc1, enc2, enc3, enc4 = "";
-    var i = 0;
-
-    // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-    var base64test = /[^A-Za-z0-9\+\/\=]/g;
-    if (base64test.exec(input)) {
-        alert("There were invalid base64 characters in the input text.\n" + "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" + "Expect errors in decoding.");
+        if (!b) {
+            b3 = b4 = 64;
+        } else if (!c) {
+            b4 = 64;
+        }
+        result += characters.charAt(b1) + characters.charAt(b2) + characters.charAt(b3) + characters.charAt(b4);
     }
-    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+    return result;
+});
 
-    do {
-        enc1 = keyStr.indexOf(input.charAt(i++));
-        enc2 = keyStr.indexOf(input.charAt(i++));
-        enc3 = keyStr.indexOf(input.charAt(i++));
-        enc4 = keyStr.indexOf(input.charAt(i++));
+// decoder
+window.atob || (window.atob = function(string) {
+    string = string.replace(/=+$/, '');
+    var a, b, b1, b2, b3, b4, c, i = 0, len = string.length, chars = [];
 
-        chr1 = (enc1 << 2) | (enc2 >> 4);
-        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-        chr3 = ((enc3 & 3) << 6) | enc4;
+    if (len % 4 === 1)
+        throw INVALID_CHARACTER_ERR;
 
-        output = output + String.fromCharCode(chr1);
+    while (i < len) {
+        b1 = characters.indexOf(string.charAt(i++));
+        b2 = characters.indexOf(string.charAt(i++));
+        b3 = characters.indexOf(string.charAt(i++));
+        b4 = characters.indexOf(string.charAt(i++));
 
-        if (enc3 != 64) {
-            output = output + String.fromCharCode(chr2);
-        }
-        if (enc4 != 64) {
-            output = output + String.fromCharCode(chr3);
-        }
+        a = ((b1 & 0x3F) << 2) | ((b2 >> 4) & 0x3);
+        b = ((b2 & 0xF) << 4) | ((b3 >> 2) & 0xF);
+        c = ((b3 & 0x3) << 6) | (b4 & 0x3F);
 
-        chr1 = chr2 = chr3 = "";
-        enc1 = enc2 = enc3 = enc4 = "";
+        chars.push(fromCharCode(a));
+        b && chars.push(fromCharCode(b));
+        c && chars.push(fromCharCode(c));
+    }
+    return chars.join('');
+});
 
-    } while (i < input.length);
-
-    return unescape(output);
-}
 
 ExcellentExport = (function() {
     var version = "1.3";
     var uri = {excel: 'data:application/vnd.ms-excel;base64,', csv: 'data:application/csv;base64,'};
     var template = {excel: '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'};
     var base64 = function(s) {
-        if (window.btoa) {
-            return window.btoa(unescape(encodeURIComponent(s)));
-        } else {
-            // Fix for IE6, IE7, IE8, IE9
-            return encode64(unescape(encodeURIComponent(s)));
-        }
+        return window.btoa(unescape(encodeURIComponent(s)));
     };
     var format = function(s, c) {
         return s.replace(/{(\w+)}/g, function(m, p) {
