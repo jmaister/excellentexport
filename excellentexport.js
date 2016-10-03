5b11bc17608d70cc62c79a61483b7d66745a4eb8
Jordi Burgos <jordiburgos@gmail.com>
2016-10-03 23:29:10 +0100

    Change version number.

--- excellentexport.js
+++ excellentexport.js
@@ -44,7 +44,7 @@ var ExcellentExport = (function() {
         return blob;
     }
 
-    var version = "1.5.0";
+    var version = "2.0.0";
     var uri = {excel: 'data:application/vnd.ms-excel;base64,', csv: 'data:application/csv;base64,'};
     var template = {excel: '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta name=ProgId content=Excel.Sheet> <meta name=Generator content="Microsoft Excel 11"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'};
     var csvDelimiter = ",";
