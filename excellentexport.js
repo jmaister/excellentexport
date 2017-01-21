e8eafe6030cb9f425f715c03782af733af956fd9
Jordi Burgos <jordiburgos@gmail.com>
2017-01-21 12:57:11 +0000

    Fix export module.

--- excellentexport.js
+++ excellentexport.js
@@ -1,5 +1,5 @@
 /**
- * ExcellentExport 2.0.2
+ * ExcellentExport 2.0.3
  * A client side Javascript export to Excel.
  *
  * @author: Jordi Burgos (jordiburgos@gmail.com)
@@ -44,7 +44,7 @@ var ExcellentExport = (function() {
         return blob;
     }
 
-    var version = "2.0.2";
+    var version = "2.0.3";
     var uri = {excel: 'data:application/vnd.ms-excel;base64,', csv: 'data:application/csv;base64,'};
     var template = {excel: '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta name=ProgId content=Excel.Sheet> <meta name=Generator content="Microsoft Excel 11"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'};
     var csvDelimiter = ",";
@@ -115,14 +115,15 @@ var ExcellentExport = (function() {
     }
 
     var ee = {
-        /** @export */
+        version: function() {
+            return version;
+        },
         excel: function(anchor, table, name) {
             table = get(table);
             var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML};
             var b64 = base64(format(template.excel, ctx));
             return createDownloadLink(anchor, b64, 'application/vnd.ms-excel','export.xls');
         },
-        /** @export */
         csv: function(anchor, table, delimiter, newLine) {
             if (delimiter !== undefined && delimiter) {
                 csvDelimiter = delimiter;
