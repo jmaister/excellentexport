06f30acf31cfadf4849f890332d106176b726123
Daniel_B <daniel.bogucki2@gmail.com>
2020-07-21 01:47:52 +0200

    Fix problem with node inside td element

--- excellentexport.js
+++ excellentexport.js
@@ -96,7 +96,7 @@ const ExcellentExport = function() {
     const tableToArray = function(table) {
         var tableInfo = Array.prototype.map.call(table.querySelectorAll('tr'), function(tr) {
             return Array.prototype.map.call(tr.querySelectorAll('th,td'), function(td) {
-                return td.innerHTML;
+                return td.firstChild && td.firstChild.nodeType !== 3 ? td.firstChild.innerHTML : td.innerHTML;
             });
         });
         return tableInfo;
