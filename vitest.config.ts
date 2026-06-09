9e7b379ecbcc47d5699dd8257d8dd60098dd3af6
Jordi Burgos <jordiburgos@gmail.com>
2026-06-10 00:12:08 +0200

    Fix github actions, fix coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,5 +18,8 @@ export default defineConfig({
                 { browser: 'chromium' },
             ],
         },
+        coverage: {
+            reporter: ['text', 'lcov'],
+        },
     },
 })
